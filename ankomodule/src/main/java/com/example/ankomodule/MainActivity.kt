1d34be7fb0fc026c3d4461f5fcc7df9f727143d8
intel <ashkhodke@gmail.com>
2018-01-17 22:37:43 +0530

    custom Dialog

--- ankomodule/src/main/java/com/example/ankomodule/MainActivity.kt
+++ ankomodule/src/main/java/com/example/ankomodule/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
@@ -60,11 +61,32 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                 }
             }
+            button("Custom Dialog"){
+                onClick{
+                    alert{
+                        /*customView {
+                            verticalLayout {
+                                editText()
+                                val nm = editText()
+                                button("Go") {
+                                    onClick{
+                                        toast(""" Go Clicked ${nm.text}""")
+                                    }
+                                }
+                            }
+                        }*/
+                        customView= View.inflate(this@MainActivity,R.layout.lgn,null)
+
+                    }.show()
+                        }
+
+                    }
+
+                }
+            }
 
 
         }
-    }
-}
 
 
 
